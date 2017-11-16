1e0e16981fe55e95f08ec11c9e4732586df5c374
marinaleikin <mleikin@hotmail.com>
2017-11-15 23:47:39 -0500

    Added files via upload

---
+++ src/Reddit.js
@@ -0,0 +1,76 @@
+/**
+ * Created by 245985 on 11/15/2017.
+ */
+import React, {Component} from 'react';
+import {redditPosts} from './static-data';
+import RedditListing from './RedditListing';
+
+export default class Reddit extends Component {
+    state = {
+        posts: {}
+    };
+    componentDidMount() {
+        // To use static data, uncomment this line:
+        this.processPosts(redditPosts.data.children);
+    }
+
+    processPosts = (posts) => {
+        // Make the data nicer to work with
+        let postsHash = posts.reduce((hash, post) => {
+            hash[post.data.id] = post.data;
+            return hash;
+        }, {});
+        this.setState({
+            posts: postsHash
+        });
+    }
+    handleDownvote = (postId) => {
+        // Immutably update the post:
+        this.setState({
+            // Set 'posts' to...
+            posts: {
+                // Every existing post, but then...
+                ...this.state.posts,
+                // Replace the one at 'postId' with...
+                [postId]: {
+                    // All the keys/values it originally had, but...
+                    ...this.state.posts[postId],
+                    // Replace its 'score' with a new value
+                    score: this.state.posts[postId].score - 1
+                }
+            }
+        });
+    }
+
+    handleUpvote = (postId) => {
+        // Immutably update the post:
+        this.setState({
+            // Set 'posts' to...
+            posts: {
+                // Every existing post, but then...
+                ...this.state.posts,
+                // Replace the one at 'postId' with...
+                [postId]: {
+                    // All the keys/values it originally had, but...
+                    ...this.state.posts[postId],
+                    // Replace its 'score' with a new value
+                    score: this.state.posts[postId].score + 1
+                }
+            }
+        });
+    }
+
+    render() {
+        // console.log(Object.keys(this.state.posts));
+        // console.log(Object.keys(this.state.posts)
+        //     .map(id => this.state.posts[id]));
+        const posts = Object.keys(this.state.posts)
+            .map(id => this.state.posts[id]);
+
+        return (
+            <RedditListing posts={posts}
+                           onUpvote={this.handleUpvote}
+                           onDownvote={this.handleDownvote} />
+        );
+    }
+}
